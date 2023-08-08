49fb4b813ed2ad9900d6e01ba8c45caec1e491d3
zekun.jin <zekun.jin@connext.com.cn>
2023-08-08 13:50:41 +0800

    chore: rm parse server json

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,13 @@
 use std::env;
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Server {
-    id: String,
-    address: String,
-    #[serde(rename = "subscriptionId")]
-    subscription_id: String,
-    #[serde(rename = "createdAt")]
-    created_at: String,
-    #[serde(rename = "updatedAt")]
-    updated_at: String,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let url = env::var("SUBSPROXY_URL").unwrap();
-    let servers = reqwest::get(&url).await?.json::<Vec<Server>>().await?;
+    let servers = reqwest::get(&url).await?.text().await?;
 
-    for server in servers {
-        println!("ID: {}", server.id);
-        println!("Address: {}", server.address);
-        println!("Subscription ID: {}", server.subscription_id);
-        println!("Created At: {}", server.created_at);
-        println!("Updated At: {}", server.updated_at);
-        println!();
-    }
+    println!("{}", servers);
 
     Ok(())
 }
