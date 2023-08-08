72ebd02f319634823ba65ade9ccc876c27574fd9
zekun.jin <zekun.jin@connext.com.cn>
2023-08-08 17:12:44 +0800

    chore: use load_subsproxy_url

--- src/common.rs
+++ src/common.rs
@@ -1,20 +1,30 @@
+use dotenv::dotenv;
 use reqwest::Client;
+use std::env;
 
-pub fn load_version () -> &'static str {
-  return option_env!("CARGO_PKG_VERSION").unwrap();
+pub fn load_subsproxy_url() -> String {
+    dotenv().ok();
+    return env::var("SUBSPROXY_URL").unwrap();
 }
 
-pub async fn fetch_subsproxy_xray_config_text (url: &str) -> Result<String, String> {
-  let client = Client::new();
-  let version = load_version();
-  let ua = format!("{}/{}", "Subsproxy", version);
-  let response = client.get(url).header("User-Agent", ua).send().await.unwrap();
+pub fn load_version() -> &'static str {
+    return option_env!("CARGO_PKG_VERSION").unwrap();
+}
 
-  if response.status().is_success() {
-    let servers = response.text().await.unwrap();
+pub async fn fetch_subsproxy_xray_config_text(url: &str) -> Result<String, String> {
+    let client = Client::new();
+    let version = load_version();
+    let ua = format!("{}/{}", "Subsproxy", version);
+    let response = client
+        .get(url)
+        .header("User-Agent", ua)
+        .send()
+        .await
+        .unwrap();
 
-    Ok(servers)
-  } else {
-    Err(String::from("{}"))
-  }
+    if response.status().is_success() {
+        Ok(response.text().await.unwrap())
+    } else {
+        Err(String::from("{}"))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use dotenv::dotenv;
-use std::env;
-
 mod common;
-use common::fetch_subsproxy_xray_config_text;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let url = env::var("SUBSPROXY_URL").unwrap();
-    let servers = fetch_subsproxy_xray_config_text(&url).await?;
+    let url = common::load_subsproxy_url();
+    let servers = common::fetch_subsproxy_xray_config_text(&url).await?;
     println!("{}", servers);
 
     Ok(())
