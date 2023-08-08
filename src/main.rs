24d28245a837359564da5288f6e408206f05819b
zekun.jin <zekun.jin@connext.com.cn>
2023-08-08 17:03:57 +0800

    chore: rm unused mod

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ use std::env;
 mod common;
 use common::fetch_subsproxy_xray_config_text;
 
-mod client;
-use client::v2ray::parse_v2ray_config;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let url = env::var("SUBSPROXY_URL").unwrap();
     let servers = fetch_subsproxy_xray_config_text(&url).await?;
-    let v2ray = parse_v2ray_config(servers);
-    println!("{:?}", v2ray);
+    println!("{}", servers);
 
     Ok(())
 }
