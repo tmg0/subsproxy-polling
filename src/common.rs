51047c5f9ed2a7589eea12b3c49a8ebfe56eb0e8
zekun.jin <zekun.jin@connext.com.cn>
2023-08-08 17:49:05 +0800

    chore: ensure file

--- src/common.rs
+++ src/common.rs
@@ -1,17 +1,22 @@
-use dotenv::dotenv;
 use reqwest::Client;
 use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
 
 pub fn load_subsproxy_url() -> String {
-    dotenv().ok();
     return env::var("SUBSPROXY_URL").unwrap();
+}
+
+pub fn load_xray_config_filepath() -> String {
+    return env::var("XRAY_CONFIG_FILEPATH").unwrap();
 }
 
 pub fn load_version() -> &'static str {
     return option_env!("CARGO_PKG_VERSION").unwrap();
 }
 
-pub async fn fetch_subsproxy_xray_config_text(url: &str) -> Result<String, String> {
+pub async fn fetch_subsproxy_xray_config_text(url: &str) -> Result<String, ()> {
     let client = Client::new();
     let version = load_version();
     let ua = format!("{}/{}", "Subsproxy", version);
@@ -25,6 +30,30 @@ pub async fn fetch_subsproxy_xray_config_text(url: &str) -> Result<String, Strin
     if response.status().is_success() {
         Ok(response.text().await.unwrap())
     } else {
-        Err(String::from("{}"))
+        Err(())
+    }
+}
+
+pub fn ensure_file(file_path: &str) {
+    if fs::metadata(file_path).is_err() {
+        ensure_dir(filepath(file_path));
+
+        let mut file = fs::File::create(file_path).unwrap();
+        file.write_all(b"").unwrap();
+    }
+}
+
+pub fn ensure_dir(dir_path: &str) {
+    if !fs::metadata(dir_path).is_ok() {
+        fs::create_dir_all(dir_path).unwrap();
+    }
+}
+
+pub fn filepath(file_path: &str) -> &str {
+    let path = Path::new(file_path);
+    if let Some(parent) = path.parent() {
+        parent.to_str().unwrap_or("")
+    } else {
+        ""
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use dotenv::dotenv;
+
 mod common;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
     let url = common::load_subsproxy_url();
-    let servers = common::fetch_subsproxy_xray_config_text(&url).await?;
+
+    let servers = common::fetch_subsproxy_xray_config_text(&url)
+        .await
+        .unwrap();
+
     println!("{}", servers);
+
+    let xray_config_filepath = common::load_xray_config_filepath();
+
+    common::ensure_file(&xray_config_filepath);
 
     Ok(())
 }
