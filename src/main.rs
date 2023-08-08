be80bde9a6715343ca943dd669e8c137e65d1fcd
zekun.jin <zekun.jin@connext.com.cn>
2023-08-08 14:11:16 +0800

    chore: add user agent

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use std::env;
 use dotenv::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let url = env::var("SUBSPROXY_URL").unwrap();
-    let servers = reqwest::get(&url).await?.text().await?;
+    let client = reqwest::Client::new();
+    let version: &str = option_env!("CARGO_PKG_VERSION").unwrap();
+    let ua = format!("{}/{}", "Subsproxy", version);
+    let response = client.get(&url).header("User-Agent", ua).send().await?;
 
-    println!("{}", servers);
+    if response.status().is_success() {
+        let servers = response.text().await?;
+
+        println!("{}", servers);
+    }
 
     Ok(())
 }
